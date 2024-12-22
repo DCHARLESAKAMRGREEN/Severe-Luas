27fbdfbf468763279935b0ca41f3158e50478546
DCHARLESAKAMRGREEN <54727258+DCHARLESAKAMRGREEN@users.noreply.github.com>
2024-12-21 23:23:23 -0500

    Update Autotime.lua

--- Autotime.lua
+++ Autotime.lua
@@ -27,7 +27,7 @@ while true do
         elseif ping >= 60 then
             powerValue = 69.5 - 5.25
         elseif ping >= 55 then
-            powerValue = 69.5 - 4.45
+            powerValue = 69.5 - 4.75
         elseif ping >= 50 then
             powerValue = 69.5 - 4.15
         elseif ping >= 45 then
