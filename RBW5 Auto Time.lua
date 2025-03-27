233ea557c2cc850fd4b43a4d88ca4d905a3d9ece
DCHARLESAKAMRGREEN <54727258+DCHARLESAKAMRGREEN@users.noreply.github.com>
2025-03-27 14:03:11 -0400

    Update RBW5 Auto Time.lua

--- RBW5 Auto Time.lua
+++ RBW5 Auto Time.lua
@@ -4,52 +4,35 @@ local Character = getname(Player)
 local Threshold = 1
 
 local Adjustments = {
-    {Ping = 85, Value = 0.5345},
-    {Ping = 75, Value = 0.5735},
-    {Ping = 62, Value = 0.6375},
-    {Ping = 51, Value = 0.6950},
-    {Ping = 40, Value = 0.7430},
-    {Ping = 25, Value = 0.7715}
+    {Ping = 95, Value = 0.4500},
+    {Ping = 85, Value = 0.5400},
+    {Ping = 70, Value = 0.5675},
+    {Ping = 62, Value = 0.6185},
+    {Ping = 52, Value = 0.6950},
+    {Ping = 40, Value = 0.7385},
+    {Ping = 25, Value = 0.7735}
 }
 
 table.sort(Adjustments, function(a, b) return a.Ping > b.Ping end)
 
-local getping = getping
-local getvalue = getvalue
-local typeof = typeof
+local Properties = findfirstchild(findfirstchild(Workspace, Character), "Properties")
+local ShotMeter = findfirstchild(Properties, "ShotMeter")
 
-local function GetAdjustments(Ping)
-    for i = 1, #Adjustments do
-        if Ping > Adjustments[i].Ping then
-            return Adjustments[i].Value
-        end
-    end
-end
-
-local function Calculations()
-    while true do
-        local Ping = getping()
-        if Ping then
-            local Value = GetAdjustments(Ping)
-            if Value then
-                Threshold = Value
+while true do
+    local Ping = getping()
+    if Ping then
+        for i = 1, #Adjustments do
+            if Ping > Adjustments[i].Ping then
+                Threshold = Adjustments[i].Value
+                break
             end
         end
-        wait(0.1)
     end
-end
 
-local function ShotMeter()
-    local ShotMeter = findfirstchild(findfirstchild(findfirstchild(Workspace, Character), "Properties"), "ShotMeter")
-
-    while true do
-        local ShotValue = getvalue(ShotMeter)
-        if typeof(ShotValue) == "number" and ShotValue ~= 2 and ShotValue >= Threshold then
-            keyrelease(0x45)
-        end
-        wait(0)
+    local ShotValue = getvalue(ShotMeter)
+    if typeof(ShotValue) == "number" and ShotValue ~= 2 and ShotValue >= Threshold then
+        keyrelease(0x45)
     end
-end
 
-spawn(Calculations)
-ShotMeter()
+    wait()
+end
