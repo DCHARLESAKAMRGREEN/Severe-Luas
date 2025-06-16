b30c44840cdb50c37a44cf3010986fb3ca5699d0
DCHARLESAKAMRGREEN <54727258+DCHARLESAKAMRGREEN@users.noreply.github.com>
2025-06-16 19:33:20 -0400

    Update RBW5 Auto Time.lua

--- RBW5 Auto Time.lua
+++ RBW5 Auto Time.lua
@@ -4,20 +4,36 @@ local Adjustments = {
     {Ping = 100, Value = 0.6515},
     {Ping = 90, Value = 0.6653},
     {Ping = 80, Value = 0.6960}, 
-    {Ping = 70, Value = 0.7235},
-    {Ping = 60, Value = 0.7423},
-    {Ping = 50, Value = 0.7983},
+    {Ping = 70, Value = 0.7240},
+    {Ping = 60, Value = 0.7430},
+    {Ping = 50, Value = 0.8070},
     {Ping = 40, Value = 0.8350},
     {Ping = 25, Value = 0.86226}
 }
 
-table.sort(Adjustments, function(a, b) return a.Ping > b.Ping end)
+local function Rescan(Parent, Name)
+    local Result
+    repeat
+        Result = findfirstchild(Parent, Name)
+        wait(1)
+    until Result
+    return Result
+end
 
-local Character = waitforchild(Workspace, getname(getlocalplayer()))
-local Properties = waitforchild(Character, "Properties")
-local ShotMeter = waitforchild(Properties, "ShotMeter")
+table.sort(Adjustments, function(A, B) return A.Ping > B.Ping end)
+
+local Player = getname(getlocalplayer())
+local Character = Rescan(Workspace, Player)
+local Properties = Rescan(Character, "Properties")
+local ShotMeter = Rescan(Properties, "ShotMeter")
 
 while true do
+    if not Character then
+        Character = Rescan(Workspace, Player)
+        Properties = Rescan(Character, "Properties")
+        ShotMeter = Rescan(Properties, "ShotMeter")
+    end
+
     local Ping = getping()
     if Ping then
         for i = 1, #Adjustments do
@@ -26,11 +42,10 @@ while true do
                 break
             end
         end
-        wait()
     end
 
     local ShotValue = getvalue(ShotMeter)
-    if ShotValue ~= 2 and ShotValue >= Threshold then
+    if ShotValue and ShotValue ~= 2 and ShotValue >= Threshold then
         keyrelease(0x45)
     end
 
